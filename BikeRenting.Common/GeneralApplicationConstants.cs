5fcda2081d8c9e787806ba10de02cc2091ab0e42
vikto <vikto@DESKTOP-VSMI7MP>
2023-07-17 10:21:09 +0300

    Add project files.

---
+++ BikeRenting.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRenting.Common
+{
+    public static class GeneralApplicationConstants
+    {
+        public const int ReleaseYear = 2023;
+    }
+}
---
+++ BikeRenting.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+using BikeRenting.Web.ViewModels.Home;
+
+namespace BikeRenting.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
