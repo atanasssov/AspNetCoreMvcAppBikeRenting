c08289b7874716ace9e0443d798b3ba930e963af
vikto <vikto@DESKTOP-VSMI7MP>
2023-07-17 14:27:15 +0300

    Seeded categories of bikes.

--- BikeRenting.Common/EntityValidationConstants.cs
+++ BikeRenting.Common/EntityValidationConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BikeRenting.Common
+﻿namespace BikeRenting.Common
 {
     public static  class EntityValidationConstants
     {
---
+++ BikeRenting.Data/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using BikeRenting.Data.Models;
+
+namespace BikeRenting.Data.Configurations
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(this.GenerateCategories());
+        }
+
+        private Category[] GenerateCategories()
+        {
+            ICollection<Category> categories = new HashSet<Category>();
+
+            Category category;
+
+            category = new Category
+            {
+                Id = 1,
+                Name = "Road bike"
+            };
+            categories.Add(category);
+
+            category = new Category
+            {
+                Id = 2,
+                Name = "Mountain bike"
+            };
+            categories.Add(category);
+
+            category = new Category
+            {
+                Id = 3,
+                Name = "Touring bike"
+            };
+            categories.Add(category);
+
+
+
+
+            return categories.ToArray();
+        }
+    }
+}
