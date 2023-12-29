54267cae3a509cd4228f109fc583652bf3e2ff5f
vikto <v.atanassovv@gmail.com>
2023-12-29 02:01:40 +0200

    Added SweatAlert2 and confirmation when deleting a bike.

---
+++ BikeRenting.Web/wwwroot/js/deleteBike.js
@@ -0,0 +1,14 @@
+﻿function displayAlert(event) {
+    event.preventDefault();
+
+    Swal.fire({
+        title: 'Are you sure that you want to delete this bike?',
+        icon: 'question',
+        showCancelButton: true,
+    }).then(result => {
+        if (result.isConfirmed) {
+            const deletingForm = document.getElementById('deletingForm');
+            deletingForm.submit();
+        }
+    });
+}
